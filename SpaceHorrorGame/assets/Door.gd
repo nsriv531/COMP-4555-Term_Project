4d463b962fb9b653f3ab577bd81b3a0f5fc32d2f
emilyallerdings <skittlesanator@gmail.com>
2023-10-24 22:22:00 -0600

    stuff

--- SpaceHorrorGame/assets/Door.gd
+++ SpaceHorrorGame/assets/Door.gd
@@ -3,6 +3,17 @@ extends Node3D
 @onready var near = false
 @onready var playAnimation = $AnimationTree.get("parameters/playback")
 
+var open = false
+
+func onButtonPress():
+	if open:
+		playAnimation.travel("Door_Closing")
+		open = false
+	else:
+		playAnimation.travel("Door_Opening")
+		open = true
+	
+
 func doorPlay(nearArea):
 	
 	if(nearArea):
@@ -12,14 +23,16 @@ func doorPlay(nearArea):
 
 
 func _on_area_3d_body_entered(body):
-	if body.name == "Player":
-		print("Player inside area")
-		near = true
-		doorPlay(near)
+	pass
+	#if body.name == "Player":
+	#	print("Player inside area")
+	#	near = true
+	#	doorPlay(near)
 
 
 func _on_area_3d_body_exited(body):
-	if body.name == "Player":
-		print("Player outside area")
-		near = false
-		doorPlay(near)
+	pass
+	#if body.name == "Player":
+	#	print("Player outside area")
+	#	near = false
+	#	doorPlay(near)
