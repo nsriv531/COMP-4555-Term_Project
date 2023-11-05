297bc4f99bff3bd904b506fac3d9390e41ebafb2
emilyallerdings <skittlesanator@gmail.com>
2023-11-04 23:41:33 -0600

    door sounds

--- SpaceHorrorGame/assets/Door.gd
+++ SpaceHorrorGame/assets/Door.gd
@@ -1,15 +1,24 @@
 extends Node3D
+@onready var animation_player = $AnimationPlayer
+
+@onready var door_open = $DoorOpen
+@onready var door_close = $DoorClose
 
 @onready var near = false
 @onready var playAnimation = $AnimationTree.get("parameters/playback")
 
 var open = false
 
+func _ready():
+	animation_player.speed_scale = 0.01
+
 func onButtonPress():
 	if open:
+		door_close.play(0.5)
 		playAnimation.travel("Door_Closing")
 		open = false
 	else:
+		door_open.play(0.2)
 		playAnimation.travel("Door_Opening")
 		open = true
 	
